84353ac512b49af7abd795a7f3f7db36d31bab07
gary <gary.white@lodgit.com>
2022-03-07 13:01:00 +0100

    code

---
+++ app.js
@@ -0,0 +1,8 @@
+function shortcut (string) {
+    return '';
+}
+
+console.log(shortcut('hello')); // 'hll'
+console.log(shortcut('how are you today?')); // 'hw r y tdy?'
+console.log(shortcut('complain')); // 'cmpln'
+console.log(shortcut('never')); // 'nvr'
